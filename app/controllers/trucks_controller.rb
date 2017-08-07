41bab057673fe1fa7c6f29b758a657bb763e1770
Juan Couso <juan.couso@gmail.com>
2017-08-07 19:45:45 -0300

    finish Truck Index, Truck show, Truck new

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,14 +1,34 @@
 class TrucksController < ApplicationController
 skip_before_action :authenticate_user!, only: :index
+before_action :set_truck, only: [:show, :edit, :update, :destroy]
   def index
+    @trucks = current_user.trucks
   end
 
   def show
   end
 
   def new
+    @truck = Truck.new()
+  end
+
+  def create
   end
 
   def edit
   end
+
+  def destroy
+  end
+
+  private
+
+  def set_truck
+    @truck = current_user.trucks.find(params[:id])
+  end
+
+  def truck_params
+    params.require(:truck).permit(:name, :vacancies)
+  end
+
 end
