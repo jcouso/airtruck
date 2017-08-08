c36919366298b2a6bfc5f2c7518c9747acf5e205
Lucas Mussi <lucasmussi@gmail.com>
2017-08-08 11:09:05 -0300

    mexendo no trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,34 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all
   end
 
   def show
+    @trip = Trip.find(params[:id])
   end
 
   def new
+    @truck = Truck.find(params[:truck_id])
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.truck = Truck.find(params[:truck_id])
+    if @trip.save
+      redirect_to truck_path(@truck)
+    else
+      render :new
+    end
   end
 
   def edit
+
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:city_origin_id, :city_destination_id)
   end
 end
