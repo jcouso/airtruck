18c2dd8f6c21a3d2b4ebb9da85cf4a0d22ac277e
Juan Couso <juan.couso@gmail.com>
2017-08-08 18:32:30 -0300

    fixed layout and banner

--- db/migrate/20170807192654_create_trips.rb
+++ db/migrate/20170807192654_create_trips.rb
@@ -1,7 +1,7 @@
 class CreateTrips < ActiveRecord::Migration[5.0]
   def change
     create_table :trips do |t|
-      t.string :departure_date
+      t.datetime :departure_date
       t.datetime :arrival_date
       t.datetime :departure_address
       t.string :arrival_address
