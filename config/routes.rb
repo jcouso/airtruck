84dfc7655ec80dc81100c55b1ef8d7cce05f046b
Juan Couso <juan.couso@gmail.com>
2017-08-08 14:20:37 -0300

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :trucks do
-      resources :trips, except: [:index, :show]
+      resources :trips, only: [:new, :create] do
+        collection do
+          get 'list'
+        end
+      end
     end
+    resources :trips, only: [:update, :edit, :destroy, :show, :index]
   end
   resources :trips, only: [:show]
 end
