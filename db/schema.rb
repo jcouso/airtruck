d27e2c6b51b50eb71c8a0520fc397425571eeec6
Raphael Kamm <raphaelkamm@hotmail.com>
2017-08-10 11:21:09 -0300

    migration add coordinates to cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807205859) do
+ActiveRecord::Schema.define(version: 20170810142020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170807205859) do
     t.string   "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "trips", force: :cascade do |t|
