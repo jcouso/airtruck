1369815c390eea19bdf5d642ba2d275363cbb0af
Fernando Antunes <fernandoantunes@gmail.com>
2017-08-11 12:38:32 -0300

    User updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, controllers: { registrations: 'registrations' }
   root to: 'trips#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
